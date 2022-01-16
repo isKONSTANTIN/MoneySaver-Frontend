49adac154abf1710b1f44eadba47244b2d0e2289
isKONSTANTIN <me@m.knst.su>
2022-01-16 12:49:02 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -12,11 +12,8 @@ cp ./package.json deploy/
 
 echo "#!/bin/bash" >> deploy/start.sh
 echo "" >> deploy/start.sh
-echo "cd ~" >> deploy/start.sh
+echo "cd ~/server/" >> deploy/start.sh
 echo "npm i" >> deploy/start.sh
 echo "NUXT_HOST=0.0.0.0 BASE_URL=${BASE_URL} npm start" >> deploy/start.sh
 
 chmod +x deploy/start.sh
-
-zip -r deploy.zip deploy/
-rm -r deploy/
