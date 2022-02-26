8d93e0dd1ab1cd9c655afd05b2153bbde7a2189c
kostya1661@gmail.com <!#%&(@$^*)lokin135kostya!#%&(@$^*)>
2022-02-26 22:10:52 +0300

    Fix base url

--- building/build.sh
+++ building/build.sh
@@ -11,6 +11,6 @@ cp ./package.json building/
 echo "#!/bin/bash" >> building/start.sh
 echo "" >> building/start.sh
 echo "npm i" >> building/start.sh
-echo "NUXT_HOST=0.0.0.0 npm start" >> building/start.sh
+echo "NUXT_HOST=0.0.0.0 BASE_URL=$BASE_URL npm start" >> building/start.sh
 
 chmod +x building/start.sh
