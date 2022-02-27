a853b9ad97789bd2680ac47afaff380d11c48f90
kostya1661@gmail.com <!#%&(@$^*)lokin135kostya!#%&(@$^*)>
2022-02-27 17:08:28 +0300

    And again try fix auth middleware

--- middleware/nonauthed.js
+++ middleware/nonauthed.js
@@ -7,20 +7,13 @@ export default async function ({app, context, redirect, store}) {
 
   var user = undefined
 
-  await fetch("http://backend/api/user?token=" + session)
-    .then(response => {
-      if (!response.ok)
-        throw Error(response.statusText);
-
-      return response
-    })
-    .then(response => response.json())
+  await app.$axios.get("api/user?token=" + session)
     .then(result => {
       store.commit("setUser", result);
       app.$cookies.set("auth_session", session,{maxAge: 60 * 60 * 24 * 7});
       user = result;
     }).catch(e => {
-      console.log(e)
+
     })
 
   if (user !== undefined){
